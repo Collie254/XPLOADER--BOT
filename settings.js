2f17e300026186cf8bb75c8335ac526742c87f06
Collie254 <colliericchie@gmail.com>
2025-02-04 11:50:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -58,7 +58,7 @@ global.mode = process.env.MODE || 'public';
 // Set to 'private' to enable private mode, otherwise default is 'public'
 
 //=================================================//
-global.statusemoji = process.env.STATUS_EMOJI || '👹'
+global.statusemoji = process.env.STATUS_EMOJI || '🥵'
 //Enter the emoji that you wish to be reacted to statuses 
 
 //=================================================//
